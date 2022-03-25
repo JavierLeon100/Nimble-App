cace05b8f80d5e015c117917cd275041843f2965
JavierLeon100 <lleonjjavier@gmail.com>
2022-03-24 19:26:28 -0700

    Added Apollo Client Pckg

--- client/App.js
+++ client/App.js
@@ -1,14 +1,26 @@
 import { NativeBaseProvider, extendTheme, Image } from "native-base";
-import { useFonts, Quicksand_300Light, Quicksand_400Regular, Quicksand_600SemiBold, Quicksand_500Medium } from "@expo-google-fonts/quicksand";
-import AppLoading from 'expo-app-loading'
+import {
+    useFonts,
+    Quicksand_300Light,
+    Quicksand_400Regular,
+    Quicksand_600SemiBold,
+    Quicksand_500Medium,
+} from "@expo-google-fonts/quicksand";
+import AppLoading from "expo-app-loading";
 import OnBoardingScreen from "./OnBoardingScreen";
 import { createContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-export const fontsContext = createContext()
+// Initialize Apollo Client
+const client = new ApolloClient({
+    uri: "localhost:4000/graphql",
+    cache: new InMemoryCache(),
+});
+
+export const fontsContext = createContext();
 
 export default function App() {
-    
     let [fontsLoaded] = useFonts({
         Quicksand_300Light,
         Quicksand_400Regular,
@@ -29,23 +41,23 @@ export default function App() {
                 lightBlueS: "#B5B6FF",
                 blueS: "#5556CC",
             },
-            secondary : "#FF6C58",
-            accent : "#FBD300",
-            white : "#FEFEFE",
-            black : "#0B0B19",
-            darkBlueGray : "#F0F0FF",
-            blueGray : "#F5F5FC",
-            mainGray : "#E9E9E9",
-            backGroundLightBlue  : "#F7F7FF",
-            backGroundModal : "#F5F5FC",
-            cancelBtnGray : "#858585",
-            eggYellow : "#FBD300",
-            red : "#FF0000",
-            schemaForBtn : {
-                400 : "#6A6CFF"
+            secondary: "#FF6C58",
+            accent: "#FBD300",
+            white: "#FEFEFE",
+            black: "#0B0B19",
+            darkBlueGray: "#F0F0FF",
+            blueGray: "#F5F5FC",
+            mainGray: "#E9E9E9",
+            backGroundLightBlue: "#F7F7FF",
+            backGroundModal: "#F5F5FC",
+            cancelBtnGray: "#858585",
+            eggYellow: "#FBD300",
+            red: "#FF0000",
+            schemaForBtn: {
+                400: "#6A6CFF",
             },
-            loginFormBlue : "#4E53FB40",
-            childBackGround : "#FFFAF9"
+            loginFormBlue: "#4E53FB40",
+            childBackGround: "#FFFAF9",
         },
         fontConfig: {
             Quicksand: {
@@ -70,19 +82,17 @@ export default function App() {
             body: "Quicksand_500Medium",
             head: "Quicksand_600SemiBold",
         },
+    });
 
-    })
-
-    
-
-    
     return (
-        <NavigationContainer>
-            <NativeBaseProvider theme={theme}>
-                <fontsContext.Provider value={fontsContext}>
-                    <OnBoardingScreen fontsLoaded={fontsLoaded}/>
-                </fontsContext.Provider>
-            </NativeBaseProvider>
-        </NavigationContainer>
+        <ApolloProvider client={client}>
+            <NavigationContainer>
+                <NativeBaseProvider theme={theme}>
+                    <fontsContext.Provider value={fontsContext}>
+                        <OnBoardingScreen fontsLoaded={fontsLoaded} />
+                    </fontsContext.Provider>
+                </NativeBaseProvider>
+            </NavigationContainer>
+        </ApolloProvider>
     );
 }
